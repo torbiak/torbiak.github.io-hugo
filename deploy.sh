6c9ead3a416234e29c8b3facc3c66076f3736b65
Jordan Torbiak <torbiak@gmail.com>
2017-10-24 23:32:47 -0600

    Add description to public/ commits

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -eu
+desc=$(git log -n 1 --format='%h: %s')
 git -C public clean -xfd
 hugo --buildDrafts
 cd public
 git add -A
-git commit --amend -m 'init'
+git commit --amend -m "$desc"
 git push -f origin master
